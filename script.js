796efed65d3e145b63e233127f6a44f0e71cf5ed
ahmed jamal <a7medjamal@outlook.com>
2018-11-17 21:03:58 +0300

    variables in javascript

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-document.writeln("hi from javascript external");
+// for comment from keyboard   (ctrl+ /)
 
 
+// document.writeln("hi from javascript external");
+
+var name1=5;
+var Name="ahmed";
+var $name=45;
+var sum = $name+name1;
+
+document.writeln(sum);
 
 function myclick() {
     alert("new alert");
 }
+
